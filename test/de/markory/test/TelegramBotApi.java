e55b61eff3dc9f7cbfe35d4fe4877b12d69f61d0
Philipp Müller-Leven <admin@markory.de>
2015-07-30 11:43:31 +0200

    insert first test-case

---
+++ test/de/markory/test/TelegramBotApi.java
@@ -0,0 +1,14 @@
+package de.markory.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TelegramBotApi {
+
+	@Test
+	public void testSendRequest() {
+		fail("Not yet implemented");
+	}
+
+}
