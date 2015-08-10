006d7983aa387880ed55f4c3641c6217756100b2
Philipp Müller-Leven <admin@markory.de>
2015-08-10 20:22:14 +0200

    insert chaining for request builder

--- src/de/markory/tgbotapi/request/builder/GetUpdatesRequestBuilder.java
+++ src/de/markory/tgbotapi/request/builder/GetUpdatesRequestBuilder.java
@@ -4,10 +4,12 @@ import de.markory.tgbotapi.request.raw.GetUpdatesRequest;
 import de.markory.tgbotapi.request.types.GetUpdates;
 
 public class GetUpdatesRequestBuilder extends RequestBuilder<GetUpdates,GetUpdatesRequest>  {
-	
+
+	public GetUpdates updates = new GetUpdates();
+
 	@Override
-	public GetUpdatesRequest build(GetUpdates updates) {
-		
+	public GetUpdatesRequest build() {
+
 		final GetUpdatesRequest request = new GetUpdatesRequest();
 
 		if ( updates.getOffset() != null ) {
@@ -16,11 +18,17 @@ public class GetUpdatesRequestBuilder extends RequestBuilder<GetUpdates,GetUpdat
 		if ( updates.getLimit() != null ) {
 			request.setParameter(GetUpdatesRequest.MessageParam.limit, Integer.toString(updates.getLimit()));
 		}
-		
+
 		if ( updates.getTimeout() != null ) {
 			request.setParameter(GetUpdatesRequest.MessageParam.timeout, Integer.toString(updates.getTimeout()));
 		}
-		
+
 		return request;
+	}
+
+	@Override
+	public GetUpdatesRequestBuilder setEntity(GetUpdates entity) {
+		this.updates = entity;
+		return this;
 	}
 }
--- src/de/markory/tgbotapi/request/builder/RequestBuilder.java
+++ src/de/markory/tgbotapi/request/builder/RequestBuilder.java
@@ -1,6 +1,8 @@
 package de.markory.tgbotapi.request.builder;
 
 public abstract class RequestBuilder<E,T> {
-	
-	public abstract T build(E request);
+
+	public abstract RequestBuilder<E, T> setEntity(E entity);
+
+	public abstract T build();
 }
